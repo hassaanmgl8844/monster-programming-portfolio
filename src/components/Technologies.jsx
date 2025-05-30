723b389b755be6291aced6ee61f9c61995cf7ba7
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-05-30 15:41:04 +0500

    Working on It

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Technologies = () => {
   return (
     <section className="text-white max-h-screen flex items-center bg-black/70">
-      <div className="">Technologies</div>
+      <div className="bg-black text-green-700 border-2 ">Technologies</div>
       <div></div>
     </section>
   );
