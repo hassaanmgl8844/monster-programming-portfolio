bacb31c29df0d36927ed93765dd865980069feee
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-05-30 15:48:19 +0500

    Added a Pading in the Page on the top and Bottom

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Technologies = () => {
   return (
-    <section className="text-white h-auto w-full flex items-center justify-center bg-black/70">
+    <section className="text-white h-auto w-full flex items-center justify-center  bg-black/70 py-10">
       <div className="bg-black text-green-300 border border-green-300 rounded px-5 py-3">Technologies</div>
       <div></div>
     </section>
