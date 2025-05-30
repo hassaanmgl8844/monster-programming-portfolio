6856364e1642c777b864b167729b03c23e6809fe
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-05-30 16:02:30 +0500

    Agar bete parho ge nahi to age kaise barho Ge

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -6,11 +6,13 @@ const Technologies = () => {
       <div className="bg-black text-green-300 border-green-300 border-2 rounded px-5 py-3 md:text-2xl lg:text-3xl ">
         Technologies
       </div>
-      <div className="flex flex-wrap">
-        <div>React</div>
-        <div>Express JS</div>
-        <div>Node JS</div>
-        <div>Mongo DB</div>
+      <div className="h-full ">
+        <div className="border-2 flex flex-wrap justify-center items-center">
+          <div className="border-2 border-green-300">React</div>
+          <div>Express JS</div>
+          <div>Node JS</div>
+          <div>Mongo DB</div>
+        </div>
       </div>
     </section>
   );
