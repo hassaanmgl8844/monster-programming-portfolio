59de97c71e696f338119ac1d5211d97118b738dd
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-05-30 15:57:55 +0500

    Got You My Boy

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -2,11 +2,16 @@ import React from "react";
 
 const Technologies = () => {
   return (
-    <section className="text-white h-auto w-full flex items-center justify-center  bg-black/70 py-10">
+    <section className="text-white h-auto w-full flex items-center justify-center flex-col bg-black/70 py-10 space-y-6">
       <div className="bg-black text-green-300 border-green-300 border-2 rounded px-5 py-3 md:text-2xl lg:text-3xl ">
         Technologies
       </div>
-      <div></div>
+      <div className="flex flex-wrap">
+        <div>React</div>
+        <div>Express JS</div>
+        <div>Node JS</div>
+        <div>Mongo DB</div>
+      </div>
     </section>
   );
 };
