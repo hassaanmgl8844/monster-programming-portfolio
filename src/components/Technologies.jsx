a64dba5fafb52abe29eca8ef3fb54e63f8c404de
hassaanmgl8844 <hsnprog8844@gmail.com>
2025-05-30 15:53:47 +0500

    Technologies Section First Part Responsiveness Done

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Technologies = () => {
   return (
     <section className="text-white h-auto w-full flex items-center justify-center  bg-black/70 py-10">
-      <div className="bg-black text-green-300 border-green-300 border-2 rounded px-5 py-3 text-3xl">Technologies</div>
+      <div className="bg-black text-green-300 border-green-300 border-2 rounded px-5 py-3 md:text-2xl lg:text-3xl ">
+        Technologies
+      </div>
       <div></div>
     </section>
   );
